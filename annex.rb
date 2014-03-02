4e2507f61c06ba798436928c2b01876ccf3dc3c9
wurde <andy.bettisworth@accreu.com>
2014-03-02 13:31:34 -0600

    annex-20140302133134

--- annex.rb
+++ annex.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby -w
 # annex.rb
 # Author: Andy Bettisworth
-# Description: Sync [.script, .canvas, .template] with Annex
+# Description: Sync [.script, .canvas, .template, .rbenv] with Annex
 
 require 'optparse'
 
@@ -10,33 +10,29 @@ class Annex
   SYNC = "#{ENV['HOME']}/.sync"
   ANNEX = "/media/Annex/preseed/seed/.sync"
 
-  def remote
-    raise 'USB Annex not found.' unless File.exist?(ANNEX)
+  def sync
+    raise 'Annex not found.' unless File.exist?(ANNEX)
     commit_local
     system <<-EOF
+echo 'syncing: CANVAS';
 cd #{SYNC}/.canvas;
+git pull origin master;
 git push origin master;
+echo ''
+echo 'syncing: SCRIPT';
 cd #{SYNC}/.script;
+git pull origin master;
 git push origin master;
+echo ''
+echo 'syncing: TEMPLATE';
 cd #{SYNC}/.template;
+git pull origin master;
 git push origin master;
+echo ''
+echo 'syncing: RBENV';
 cd #{HOME}/.rbenv;
+git pull origin master;
 git push origin master;
-    EOF
-  end
-
-  def local
-    raise 'USB Annex not found.' unless File.exist?(ANNEX)
-    commit_local
-    system <<-EOF
-cd #{SYNC}/.canvas;
-git pull --rebase origin master;
-cd #{SYNC}/.script;
-git pull --rebase origin master;
-cd #{SYNC}/.template;
-git pull --rebase origin master;
-cd #{HOME}/.rbenv;
-git pull --rebase origin master;
     EOF
   end
 
@@ -60,30 +56,5 @@ git commit -m "annex-#{Time.now.strftime('%Y%m%d%H%M%S')}";
   end
 end
 
-## USAGE
-# annex --push
-# annex --pull
-
-options = {}
-option_parser = OptionParser.new do |opts|
-  executable_name = File.basename($PROGRAM_NAME, ".rb")
-  opts.banner = "Usage: #{executable_name} OPTION..."
-
-  opts.on('--pull', 'bring local repo up to date with Annex remote') do
-    options[:pull] = true
-  end
-
-  opts.on('--push', 'bring Annex up to date with local repo') do
-    options[:push] = true
-  end
-end
-option_parser.parse!
-
 update = Annex.new
-if options[:push] == true
-  update.remote
-elsif options[:pull] == true
-  update.local
-else
-  puts option_parser.help
-end
+update.sync
