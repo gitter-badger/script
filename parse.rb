f03bd198839a4d5d129028278de10a4363aeccf1
wurde <andy.bettisworth@accreu.com>
2014-05-14 18:16:23 -0500

    annex-20140514181622

---
+++ parse.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby -w
+# parse.rb
+# Author: Andy Bettisworth
+# Description: READ grammar rules of Ruby scripts
+
+require 'optparse'
+require 'ripper'
+require 'pp'
+
+raise 'WARNING: only 1 script at a time. ' if ARGV.size > 1
+
+filename = ARGV[0]
+script = File.open(filename, 'r')
+code = String.new
+script.each_line do |line|
+  code << line
+end
+
+parsed_code = Ripper.sexp(code)
+parsed_code.each do |t|
+  puts t.to_s
+end
