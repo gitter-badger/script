2e80470153f067ff190992d2a92c92c7b541a0ad
wurde <wurde@base.(none)>
2014-06-17 11:22:03 -0500

    script_clean-20140617112203

---
+++ exec_tasks.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby -w
+# exec_tasks.rb
+# Author: Andy Bettisworth
+# Description: Execute the ~/.sync/tasks
+
+class Tasks
+  HOME       = ENV['HOME']
+  TASKS_PATH = "#{HOME}/.sync/.task"
+
+  def every_minute
+    Dir.foreach(TASKS_PATH) do |task|
+      next if File.directory?(task)
+      next if task == "exec_tasks.rb"
+      eval(File.new("#{TASKS_PATH}/#{task}").read)
+    end
+  end
+end
+
+tasker = Tasks.new
+tasker.every_minute
