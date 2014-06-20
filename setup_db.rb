d325678703bf24a780ccc30b65332d52c96ad8ad
Andy Bettisworth <andy.bettisworth@gmail.com>
2014-06-20 14:31:30 -0500

    script_clean-20140620143130

---
+++ setup_db.rb
@@ -0,0 +1,10 @@
+#!/usr/binn/ruby -w
+# setup_db.rb
+# Author: Andy Bettisworth
+# Description: Setup a database using the YAML if there isn't already one
+
+class SetupDB
+  def database_exist?(db)
+  end
+end
+
