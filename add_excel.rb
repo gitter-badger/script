73e41fedcc5e284f380d1bfed0992608d72c530c
Andy Bettisworth <andy.bettisworth@gmail.com>
2014-06-21 15:29:19 -0500

    script_clean-20140621152918

--- add_excel.rb
+++ add_excel.rb
@@ -6,24 +6,30 @@
 require 'spreadsheet'
 
 class SpreadsheetFactory
-  def add_workbook(name='workbook', values=[])
+  def add_workbook(name, values=[])
     book = Spreadsheet::Workbook.new
     book.write "#{name}.xls"
   end
-
-  def add_worksheets
-  end
 end
 
-describe SpreadsheetFactory do
-  describe "#add_workbook" do
-    it "should create an excel workbook named 'coordinates.xls'" do
-      excel_factory = SpreadsheetFactory.new
-      excel_factory.add_workbook('coordinates')
-      expect(File.exist?('coordinates.xls')).to be_true
-    end
-
-    it "should create an excel worksheet named 'letters'"
-    it "should have a second worksheet with values [[1,1],[1,2],[1,3]]"
-  end
+excel_factory = SpreadsheetFactory.new
+if ARGV[0]
+  excel_factory.add_workbook(ARGV[0])
+elsif ARGV[1]
+  excel_factory.add_workbook(ARGV[0], ARGV[1])
+else
+  puts "USAGE: add_excel 'NAME' [VALUES]"
 end
+
+# describe SpreadsheetFactory do
+#   describe "#add_workbook" do
+#     it "should create an excel workbook named 'coordinates.xls'" do
+#       excel_factory = SpreadsheetFactory.new
+#       excel_factory.add_workbook('coordinates')
+#       expect(File.exist?('coordinates.xls')).to be_true
+#     end
+
+#     it "should create an excel worksheet named 'letters'"
+#     it "should have a second worksheet with values [[1,1],[1,2],[1,3]]"
+#   end
+# end
