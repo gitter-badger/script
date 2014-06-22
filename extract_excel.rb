fcbddc993578bda19af0a8add081f00a927ddd4c
Andy Bettisworth <andy.bettisworth@gmail.com>
2014-06-22 14:18:57 -0500

    script_clean-20140622141857

---
+++ extract_excel.rb
@@ -0,0 +1,43 @@
+#/usr/bin/ruby -w
+# extract_excel.rb
+# Author: Andy Bettisworth
+# Description: Read excel spreadsheet values as an Array
+
+require 'spreadsheet'
+
+class SpreadsheetArray
+  def extract(name)
+    name = default_extension(name)
+    raise "ExcelFileNotFoundError! No file named #{name}" unless File.exist?(name)
+    raise 'ExcelFileRequiredError!' unless File.extname(name) == '.xls'
+
+    all_values = []
+    book = Spreadsheet.open name
+    book.worksheets.each do |sheet|
+      sheet.each do |row|
+        row_array = []
+        row.each do |col|
+          row_array << col
+        end
+        all_values << row_array
+      end
+    end
+
+    puts all_values.inspect
+    all_values
+  end
+
+  def default_extension(excel)
+    if File.extname(excel) == ''
+      excel += '.xls'
+    end
+    excel
+  end
+end
+
+excel_array = SpreadsheetArray.new
+if ARGV[0]
+  excel_array.extract(ARGV[0])
+else
+  puts "USAGE: extract_excel NAME"
+end
