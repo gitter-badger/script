65f04f4a4dbd852d6cdf557e8a9d67f98415cc24
wurde <andy.bettisworth@accreu.com>
2014-07-04 14:03:01 -0500

    script_clean-20140704140300

---
+++ .tasks/alert_fifa_game.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby -w
+# alert_fifa_game.rb
+# Author: Andy Bettisworth
+# Description:
+
+# Netherlands vs Mexico
+if Time.now.strftime('%H:%M') == '11:00'
+
+end
+
+# Costa rica vs Greece
+if Time.now.strftime('%H:%M') == '15:00'
+
+end
