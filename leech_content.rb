d1b1f71fd39b039b56e847b837f0525b74464120
wurde <andy.bettisworth@accreu.com>
2014-07-06 21:06:13 -0500

    script_clean-20140706210613

---
+++ leech_content.rb
@@ -0,0 +1,36 @@
+#!/usr/ruby -w
+# leech_content.rb
+# Author: Andy Bettisworth
+# Description: Fetch HTML from target URL
+
+require 'erb'
+require 'nokogiri'
+require 'open-uri'
+
+class HTMLFetcher
+  TEST_TEMPLATE = 'test.html.erb'
+
+  # downloads & process web queries
+  def leech_content(url_path, nokogiri_queries = {})
+    doc = Nokogiri::HTML(open(url_path), "UTF-8")
+    nokogiri_queries.each do |key, value|
+      puts "Retrieving: #{key}..."
+      nokogiri_queries[key] = doc.xpath(value)
+      puts "Found:\n#{nokogiri_queries[key]}"
+      puts ""
+    end
+    nokogiri_queries
+  end
+
+  # creates a HTML file using the template and data provided
+  def create_html_file(filename, template, data)
+    File.open(filename, "w+:utf-8") do |file|
+      file << ERB.new(File.read(template)).result(data.to_binding)
+    end
+  end
+end
+
+fetcher = HTMLFetcher.new
+fetcher.leech_content("http://drawcrowd.com/",
+  top_left_img: '//*[@id="top-ban"]/div/div/div[1]/div/div[1]/div/img',
+  top_right_img: '//*[@id="top-ban"]/div/div/div[2]/div/div[1]/div/img')
