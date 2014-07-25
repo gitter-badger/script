f8874feafdb840618e436bde21360f6c194f326d
wurde <andy.bettisworth@accreu.com>
2014-07-24 23:30:11 -0500

    script_clean-20140724233011

---
+++ rails_analytics.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby -w
+# rails_analytics.rb
+# Author: Andy Bettisworth
+# Description: A Rails Analytics Dashboard
+
+## Technology Stack Logs
+# web - nginx
+# web - unicorn
+# app - rails
+# db  - postgresql
+# system - syslogd
+# system - snmp data
