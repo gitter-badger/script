19baba51e86c4134380770250e3ca4a96505d992
wurde <andy.bettisworth@accreu.com>
2014-07-29 23:47:11 -0500

    script_clean-20140729234711

--- cleanup_old_kernels.rb
+++ cleanup_old_kernels.rb
@@ -3,13 +3,22 @@
 # Author: Andy Bettisworth
 # Description: Remove old and unused linux kernels
 
+require 'fileutils'
+
 current_kernel = `uname -r`.chomp
-all_kernels = Dir['/lib/modules/**']
+lib_modules    = Dir['/lib/modules/**']
+boot_files     = Dir['/boot/**'].select! { |x| File.file?(x) }
 
-all_kernels.each do |kernel|
+lib_modules.each do |kernel|
   target_kernel = File.basename(kernel).chomp
-
   unless current_kernel == target_kernel
-    File.delete(kernel)
+    FileUtils.rm_rf(kernel)
+  end
+end
+
+boot_files.each do |f|
+  target_file = File.basename(f).chomp
+  unless target_file.match(/#{current_kernel}/)
+    FileUtils.rm_rf(f)
   end
 end
