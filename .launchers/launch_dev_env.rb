dbc7e18f6b277508a6f32fc1d908b5b5d0bac745
wurde <andy.bettisworth@accreu.com>
2014-09-16 22:01:19 -0500

    not much

--- .launchers/launch_dev_env.rb
+++ .launchers/launch_dev_env.rb
@@ -1,30 +1,45 @@
-#!/usr/bin/env ruby -w
 # launch_development_environment.rb
+# Author: Andy Bettisworth
 # Description: Launch development environment
 
-require_relative 'rbwindow'
+require 'wmctrl'
 
-## Setup development environment
-## Setup 2
-window = RBWindow.new
-window.close_all_windows
-window.change_workspace("top-right")
+wm = WMCtrl.new
 
-sleep(1)
-## Sublime-Text-3
-sublime_window = RBWindow.new
-sublime_window.create_application("sublime")
-sublime_window.relocate("top-right")
-sublime_window.change_window_state('add','maximize')
+## GET geometry of desktop
+desktop1 = wm.list_desktops[0]
+x_boundary = desktop1[:geometry][0]/2
+y_boundary = desktop1[:geometry][1]/2
 
-## Gnome-Terminal
-terminal_window = RBWindow.new
-terminal_window.create_application("gnome-terminal")
-terminal_window.relocate("top-left");
-terminal_window.change_window_state('add','maximize')
+## CLOSE all windows
+wm_data = wm.list_windows.map { |w| wm.get_window_data(w[:id]) }
+wm_data.reject! { |w| w[:state].include?("_NET_WM_STATE_SKIP_PAGER") }
+wm_data.reject! { |w| w[:class].include?("desktop_window.Nautilus") }
+wm_data.each { |w| wm.action_window(w[:id], :close) }
 
-## Chrome
-chrome_window = RBWindow.new
-chrome_window.create_application("google-chrome", "localhost:3000 localhost:8000")
-chrome_window.relocate("bottom-right")
-chrome_window.change_window_state('add','maximize')
+## NAVIGATE to top left workspace
+wm.change_viewport(0,0)
+
+## CREATE processes
+spawn('gnome-terminal')
+spawn('sublime')
+spawn('google-chrome')
+
+## WAIT for windows
+sleep(0.5)
+
+## OPEN terminal
+wm_terminal = wm.list_windows.select! { |w| w[:class] == "gnome-terminal.Gnome-terminal" }
+wm.action_window(wm_terminal[0][:id], :move_resize, 0, 0, 0, -1, -1)
+wm.action_window(wm_terminal[0][:id], :change_state, "add", "maximized_horz", "maximized_vert")
+
+## OPEN text editor
+wm_texteditor = wm.list_windows.select! { |w| w[:class] == "sublime.Sublime" }
+wm.action_window(wm_texteditor[0][:id], :move_resize, 0, x_boundary, 0, -1, -1)
+wm.action_window(wm_texteditor[0], :change_state, "add", "maximized_vert")
+
+## OPEN browser
+wm_browser = wm.list_windows.select! { |w| w[:class] == "Google-chrome-stable.Google-chrome-stable" }
+wm.action_window(wm_browser[0][:id], :move_resize, 0, x_boundary, y_boundary, -1, -1)
+wm.action_window(wm_browser[0], :change_state, "add", "maximized_horz", "maximized_vert")
+
--- launch_dev_env.rb
+++ launch_dev_env.rb
@@ -7,6 +7,11 @@ require 'wmctrl'
 
 wm = WMCtrl.new
 
+## GET geometry of desktop
+desktop1 = wm.list_desktops[0]
+x_boundary = desktop1[:geometry][0]/2
+y_boundary = desktop1[:geometry][1]/2
+
 ## CLOSE all windows
 wm_data = wm.list_windows.map { |w| wm.get_window_data(w[:id]) }
 wm_data.reject! { |w| w[:state].include?("_NET_WM_STATE_SKIP_PAGER") }
@@ -17,26 +22,24 @@ wm_data.each { |w| wm.action_window(w[:id], :close) }
 wm.change_viewport(0,0)
 
 ## CREATE processes
-pid1 = spawn('gnome-terminal')
-pid2 = spawn('sublime')
-pid3 = spawn('google-chrome')
+spawn('gnome-terminal')
+spawn('sublime')
+spawn('google-chrome')
 
+## WAIT for windows
 sleep(0.5)
 
 ## OPEN terminal
 wm_terminal = wm.list_windows.select! { |w| w[:class] == "gnome-terminal.Gnome-terminal" }
-wm.action_window(wm_terminal[0], :change_state, "add", "maximized_horz", "maximized_vert")
-# terminal_window.relocate("top-left");
-# terminal_window.change_window_state('add','maximize')
+wm.action_window(wm_terminal[0][:id], :move_resize, 0, 0, 0, -1, -1)
+wm.action_window(wm_terminal[0][:id], :change_state, "add", "maximized_horz", "maximized_vert")
 
 ## OPEN text editor
 wm_texteditor = wm.list_windows.select! { |w| w[:class] == "sublime.Sublime" }
+wm.action_window(wm_texteditor[0][:id], :move_resize, 0, x_boundary, 0, -1, -1)
 wm.action_window(wm_texteditor[0], :change_state, "add", "maximized_vert")
-# sublime_window.relocate("top-right")
-# sublime_window.change_window_state('add','maximize')
 
 ## OPEN browser
 wm_browser = wm.list_windows.select! { |w| w[:class] == "Google-chrome-stable.Google-chrome-stable" }
+wm.action_window(wm_browser[0][:id], :move_resize, 0, x_boundary, y_boundary, -1, -1)
 wm.action_window(wm_browser[0], :change_state, "add", "maximized_horz", "maximized_vert")
-# chrome_window.relocate("bottom-right")
-# chrome_window.change_window_state('add','maximize')
