ddc1bf99dec734d7434cc6bd4222702254f8b311
wurde <andy.bettisworth@accreu.com>
2014-09-19 20:29:52 -0500

    point to correct browser

--- .launchers/launch_dev_env.rb
+++ .launchers/launch_dev_env.rb
@@ -18,7 +18,7 @@ wm_data.reject! { |w| w[:state].include?("_NET_WM_STATE_SKIP_PAGER") }
 wm_data.reject! { |w| w[:class].include?("desktop_window.Nautilus") }
 wm_data.each { |w| wm.action_window(w[:id], :close) }
 
-sleep(1.5)
+sleep(1)
 
 wm.change_viewport(0,0)
 
@@ -27,7 +27,7 @@ spawn('gnome-terminal')
 spawn('sublime')
 spawn('google-chrome')
 
-sleep(1.5)
+sleep(1)
 
 ## OPEN terminal
 wm_terminal = wm.list_windows.select! { |w| w[:class] == "gnome-terminal.Gnome-terminal" }
@@ -40,6 +40,6 @@ wm.action_window(wm_texteditor[0][:id], :move_resize, 0, x_boundary, 0, -1, -1)
 wm.action_window(wm_texteditor[0][:id], :change_state, "add", "maximized_vert")
 
 ## OPEN browser
-wm_browser = wm.list_windows.select! { |w| w[:class] == "Google-chrome-stable.Google-chrome-stable" }
+wm_browser = wm.list_windows.select! { |w| w[:class] == "Google-chrome.Google-chrome" }
 wm.action_window(wm_browser[0][:id], :move_resize, 0, x_boundary, y_boundary, -1, -1)
 wm.action_window(wm_browser[0][:id], :change_state, "add", "maximized_horz", "maximized_vert")
