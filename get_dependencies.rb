28c6b5640481c6e2e2fdeff267f21fb62d555d87
wurde <andy.bettisworth@accreu.com>
2014-09-27 01:57:51 -0500

    working dependency getter, WOOT!

--- get_dependencies.rb
+++ get_dependencies.rb
@@ -7,24 +7,28 @@ class DependencyGetter
   def get(packages)
     raise "MissingArgumentError: Target package is required." unless packages[0]
 
-    clear_buckets
+    clear_bucket_dependencies
 
     packages.each do |pkg|
-      raise "UnknownPackageError: No matching Ubuntu package found named '#{pkg}'" if find_package(pkg) == 0
-      download_package(pkg)
-      try_install(pkg)
+      if package_exist?(pkg)
+        download_package(pkg)
+        try_install(pkg)
+      else
+        system("aptitude search #{pkg}")
+        raise "UnknownPackageError: No matching Ubuntu package found named '#{pkg}'"
+      end
     end
   end
 
   private
 
-  def clear_buckets
+  def clear_bucket_dependencies
     system('rm -rf *bucket_*')
   end
 
-  def find_package(pkg)
+  def package_exist?(pkg)
     result = `aptitude show #{pkg}`
-    return result.length
+    return result.length == 0 ? false : true
   end
 
   def download_package(pkg)
@@ -32,13 +36,39 @@ class DependencyGetter
   end
 
   def try_install(pkg)
-    system("sudo aptitude install #{pkg}")
+    result = false
+    count = 0
+
+    until result
+      result = system("sudo dpkg -i #{pkg}* 2> dependency_error")
+
+      if result
+        puts "SUCCESS! Package '#{pkg}' was installed successfully."
+      else
+        get_dependencies(read_dependencies, count)
+      end
+
+      system('rm dependency_error')
+      count += 1
+    end
   end
 
   def read_dependencies
+    error = File.open('dependency_error').read
+    dependencies = error.scan(/depends on (.*)?;/)
+    return dependencies.flatten
   end
 
-  def get_dependencies
+  def get_dependencies(dependencies, count)
+    system("mkdir dependency_bucket_#{count}")
+    Dir.chdir("dependency_bucket_#{count}")
+
+    dependencies.each do |pkg|
+      download_package(pkg)
+      try_install(pkg)
+    end
+
+    Dir.chdir('../')
   end
 end
 
