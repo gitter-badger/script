7699290352f2ca0ec9d01e24fa85a627c9148f43
wurde <andy.bettisworth@accreu.com>
2014-09-27 20:34:21 -0500

    heavy refactoring and play

--- get_package.rb
+++ get_package.rb
@@ -7,34 +7,79 @@ require 'fileutils'
 require 'open-uri'
 
 class PackageDownloader
+  attr_accessor :top_directory
+  attr_accessor :package_queue
+  attr_accessor :dependency_queue
 
-  attr_accessor :dependency_count
-  attr_accessor :target_pakage
+  attr_accessor :target_package_index
+  attr_accessor :dependency_index
 
   def get(packages)
     raise "NoNetworkConnection: Check your network configuration" unless internet_connection?
-    raise "MissingArgumentError: Target package is required." unless packages[0]
+    raise "MissingArgument: At least one target package is required." unless packages[0]
 
-    packages.each do |pkg|
-      raise "UnknownPackageError: '#{pkg}'" unless package_exist?(pkg)
+    @top_directory  = Dir.pwd
+    @package_queue = Hash[packages.flatten.each_with_index.map { |value, index| [value, false] }]
 
-      Dir.chdir("#{ENV['HOME']}/Desktop")
-      download_package(pkg)
+    @package_queue.each do |pkg, status|
+      raise "UnknownPackageError: '#{pkg}'" unless package_exist?(pkg) || package_local?(pkg)
 
-      @target_pakage    = pkg
-      @dependency_count = 0
-      is_installed      = false
-
-      until is_installed == true
-        Dir.chdir("#{ENV['HOME']}/Desktop")
-        is_installed = install_target(pkg)
-
-        get_dependencies(pkg)
+      until @package_queue[pkg] == true
+        Dir.chdir(@top_directory)
+        download_package(pkg) unless package_local?(pkg)
+        install_target(pkg)
+        # install_dependencies(pkg)
       end
     end
   end
 
   private
+
+  def download_package(pkg)
+    system("aptitude download #{pkg}")
+  end
+
+  def install_target(pkg)
+    result = system("sudo dpkg -i #{pkg}_*")
+    @package_queue[pkg] = true if result
+  end
+
+  # def install_dependencies(pkg)
+  #   system("mkdir -p dependency_bucket_#{@dir_level}")
+  #   Dir.chdir("dependency_bucket_#{@dir_level}")
+
+  #   dependencies = read_dependencies
+  #   @dir_level += 1
+
+  #   dependencies.each do |dep_pkg|
+  #     @target_pakage = dep_pkg
+  #     download_package(dep_pkg)
+
+  #     is_installed = false
+
+  #     until is_installed == true
+  #       is_installed = install_target(dep_pkg)
+
+  #       Dir.chdir("#{ENV['HOME']}/Desktop")
+  #       get_dependencies(dep_pkg)
+  #     end
+  #   end
+  # end
+
+  # def read_dependencies
+  #   error = File.open("#{ENV['HOME']}/Desktop/dependency_error#{@dir_level}_#{@target_pakage}").read
+  #   error.scan(/depends on (.*)?;/).flatten
+  # end
+
+  def package_exist?(pkg)
+    result = `aptitude show #{pkg}`
+    result.length == 0 ? false : true
+  end
+
+  def package_local?(pkg)
+    result = Dir["#{pkg}_*"].select { |f| f =~ /#{pkg}_/ }
+    result.count == 0 ? false : true
+  end
 
   def internet_connection?
     begin
@@ -42,48 +87,6 @@ class PackageDownloader
     rescue
       false
     end
-  end
-
-  def package_exist?(pkg)
-    result = `aptitude show #{pkg}`
-    result.length == 0 ? false : true
-  end
-
-  def download_package(pkg)
-    system("aptitude download #{pkg}")
-  end
-
-  def install_target(pkg)
-    result = system("sudo dpkg -i #{pkg}* 2> #{ENV['HOME']}/Desktop/dependency_error#{@dependency_count}_#{@target_pakage}")
-    result ? true : false
-  end
-
-  def get_dependencies(pkg)
-    # > Q: why are unused directories being created
-    system("mkdir -p dependency_bucket_#{@dependency_count}")
-    Dir.chdir("dependency_bucket_#{@dependency_count}")
-
-    dependencies = read_dependencies
-    @dependency_count += 1
-
-    dependencies.each do |dep_pkg|
-      @target_pakage = dep_pkg
-      download_package(dep_pkg)
-
-      is_installed = false
-
-      until is_installed == true
-        is_installed = install_target(dep_pkg)
-
-        Dir.chdir("#{ENV['HOME']}/Desktop")
-        get_dependencies(dep_pkg)
-      end
-    end
-  end
-
-  def read_dependencies
-    error = File.open("#{ENV['HOME']}/Desktop/dependency_error#{@dependency_count}_#{@target_pakage}").read
-    error.scan(/depends on (.*)?;/).flatten
   end
 end
 
