977dd0425a358a7519fbd3d8e485fa01e99d0d4a
wurde <andy.bettisworth@accreu.com>
2014-10-14 23:35:16 -0500

    cont

--- todo.rb
+++ todo.rb
@@ -20,7 +20,8 @@ class TaskManager
   def add_task(description)
     raise "No known project #{@project}" unless project_exist?(@project)
     list = get_all_tasks
-    puts largest_hash_key(list)
+    puts list
+    # puts largest_hash_key(list)
     # > CREATE todo id
     # task = [{
     #   id:
