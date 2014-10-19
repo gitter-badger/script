766888296ae3101096ba05ed5ac15fb11fff32d2
wurde <andy.bettisworth@accreu.com>
2014-10-19 09:45:43 -0500

    Getting the details down for minify.rb

---
+++ get_naics.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby -w
+# get_naics.rb
+# Author: Andy Bettisworth
+# Description: Return NAICS Code generated from user Q and A session
+
+# > GET NAICS codes
+## load NAICS Index File
+## [] Ask for 2-digit code
+## [] Ask for 3-digit code
+## [] Ask for 4-digit code
+## [] Ask for 5-digit code
+## [] Ask for 6-digit code
+
+class GetNAICS
+end
