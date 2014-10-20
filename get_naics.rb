d80b532ea6c9f7fd6c6c3022d9443e2014ffab06
wurde <andy.bettisworth@accreu.com>
2014-10-19 23:15:32 -0500

    Working get_naics script

--- get_naics.rb
+++ get_naics.rb
@@ -3,13 +3,49 @@
 # Author: Andy Bettisworth
 # Description: Return NAICS Code generated from user Q and A session
 
-# > GET NAICS codes
-## load NAICS Index File
-## [] Ask for 2-digit code
-## [] Ask for 3-digit code
-## [] Ask for 4-digit code
-## [] Ask for 5-digit code
-## [] Ask for 6-digit code
+require 'yaml'
 
-class GetNAICS
-end
+data = YAML.load_file "#{ENV['HOME']}/.sync/.template/1_features/naics_codes.yaml"
+
+two_digits   = data.select { |code, desc| code.to_s.size == 2 }
+three_digits = data.select { |code, desc| code.to_s.size == 3 }
+four_digits  = data.select { |code, desc| code.to_s.size == 4 }
+five_digits  = data.select { |code, desc| code.to_s.size == 5 }
+six_digits   = data.select { |code, desc| code.to_s.size == 6 }
+
+two_digits.each { |code, desc| puts "#{code} #{desc}" }
+puts ""
+puts "Select a 2 digit NAICS code:"
+two_code = gets.to_i until two_code
+puts ""
+
+three_digits.select! { |code| code.to_s =~ /^#{two_code}/ ? true : false }
+three_digits.each { |code, desc| puts "#{code} #{desc}" }
+puts ""
+puts "Select a 3 digit NAICS code:"
+three_code = gets.to_i
+puts ""
+
+four_digits.keep_if { |code, desc| /^#{three_code}/ =~ code.to_s }
+four_digits.each { |code, desc| puts "#{code} #{desc}" }
+puts ""
+puts "Select a 4 digit NAICS code:"
+four_code = gets.to_i until four_code
+
+five_digits.keep_if { |code, desc| /^#{four_code}/ =~ code.to_s }
+five_digits.each { |code, desc| puts "#{code} #{desc}" }
+puts ""
+puts "Select a 5 digit NAICS code:"
+five_code = gets.to_i until five_code
+
+six_digits.keep_if { |code, desc| /^#{five_code}/ =~ code.to_s }
+six_digits.each { |code, desc| puts "#{code} #{desc}" }
+puts ""
+puts "Select a 6 digit NAICS code:"
+six_code = gets.to_i until six_code
+
+puts "#{two_code} #{data[two_code]}"
+puts "#{three_code} #{data[three_code]}"
+puts "#{four_code} #{data[four_code]}"
+puts "#{five_code} #{data[five_code]}"
+puts "#{six_code} #{data[six_code]}"
