affd1eae87cceb27b226081add7d9c3ca33a2b63
wurde <andy.bettisworth@accreu.com>
2014-10-26 17:04:33 -0500

    check var

--- canvas.rb
+++ canvas.rb
@@ -66,7 +66,8 @@ class Canvas
 
   def get_canvases(pattern)
     canvas_list = Dir.entries(CANVAS).delete_if do |e|
-      File.directory?(File.join(CANVAS, e)) or !(e == '.' || e == '..' || e == ".git")
+      puts e
+      File.directory?(File.join(CANVAS, e)) and !(e == '.' || e == '..' || e == ".git")
     end
     canvas_list.select! { |s| pattern.match(s) } if pattern
     puts canvas_list
