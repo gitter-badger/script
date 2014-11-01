1ec4435dd1e692e04f1a2aa0697da390f65aa1e9
wurde <andy.bettisworth@accreu.com>
2014-10-31 22:52:54 -0500

    Add chess

---
+++ chess.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby -w
+# chess.rb
+# Author: Andy Bettisworth
+# Description: Play a game of chess at the command-line
+
+# > represent the board 8x8 64 square array
+class ChessGame
+  def play
+    board = Array.new(8, '0')
+    puts board
+  end
+end
+
+if __FILE__ == $0
+  game = ChessGame.new
+  game.play
+end
