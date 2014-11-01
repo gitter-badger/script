217c3326f6a549a77e067110576e3ddd17d36066
wurde <andy.bettisworth@accreu.com>
2014-11-01 00:53:10 -0500

    print the inputs

--- epub2mobi.py
+++ epub2mobi.py
@@ -10,25 +10,29 @@ def epub2mobi(from_dir, to_dir, ignore_if=None):
     Look for .epub files in from_dir, convert them to .mobi and store
     them in the flat directory to_dir unless their path includes any string
     present in the list ignore_if.
+
     Requires ebook-convert, coming from calibre (http://calibre-ebook.com).
-    Go to Preferences, select Miscellaneous in Advanced, and click the
-    "Install command line tools" button.
     """
-    if not os.path.exists(to_dir):
-        os.makedirs(to_dir)
-    for root, dirs, files in os.walk(from_dir):
-        ignore = False
-        if ignore_if is not None:
-            ignore = reduce(lambda a, b: a or b,
-                            [ig in root for ig in ignore_if])
-        if not ignore:
-            for fl in files:
-                name, extension = os.path.splitext(fl)
-                if extension == '.epub':
-                    mobi = os.path.join(to_dir, name + '.mobi')
-                    if not os.path.exists(mobi):
-                        os.system('ebook-convert ' +
-                                  os.path.join(root, fl) + ' ' + mobi)
+
+    print "from_dir: %s" from_dir
+    print "to_dir: %s" to_dir
+    print "ignore_if: %s" ignore_if
+
+    # if not os.path.exists(to_dir):
+    #     os.makedirs(to_dir)
+    # for root, dirs, files in os.walk(from_dir):
+    #     ignore = False
+    #     if ignore_if is not None:
+    #         ignore = reduce(lambda a, b: a or b,
+    #                         [ig in root for ig in ignore_if])
+    #     if not ignore:
+    #         for fl in files:
+    #             name, extension = os.path.splitext(fl)
+    #             if extension == '.epub':
+    #                 mobi = os.path.join(to_dir, name + '.mobi')
+    #                 if not os.path.exists(mobi):
+    #                     os.system('ebook-convert ' +
+    #                               os.path.join(root, fl) + ' ' + mobi)
 
 if __name__ == '__main__':
     import sys
