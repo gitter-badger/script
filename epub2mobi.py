4f9c79426fd3c06efa0cc90ced4a365c0789c184
wurde <andy.bettisworth@accreu.com>
2014-11-01 00:46:40 -0500

    test it

---
+++ epub2mobi.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# epub2mobi.py
+# Author: Juan Reyero
+# Description: Convert books formatted epub to mobi (kindle usage)
+
+import os
+
+def epub2mobi(from_dir, to_dir, ignore_if=None):
+    """
+    Look for .epub files in from_dir, convert them to .mobi and store
+    them in the flat directory to_dir unless their path includes any string
+    present in the list ignore_if.
+    Requires ebook-convert, coming from calibre (http://calibre-ebook.com).
+    Go to Preferences, select Miscellaneous in Advanced, and click the
+    "Install command line tools" button.
+    """
+    if not os.path.exists(to_dir):
+        os.makedirs(to_dir)
+    for root, dirs, files in os.walk(from_dir):
+        ignore = False
+        if ignore_if is not None:
+            ignore = reduce(lambda a, b: a or b,
+                            [ig in root for ig in ignore_if])
+        if not ignore:
+            for fl in files:
+                name, extension = os.path.splitext(fl)
+                if extension == '.epub':
+                    mobi = os.path.join(to_dir, name + '.mobi')
+                    if not os.path.exists(mobi):
+                        os.system('ebook-convert ' +
+                                  os.path.join(root, fl) + ' ' + mobi)
+
+if __name__ == '__main__':
+    import sys
+    from_dir, to_dir = '.', '.'
+    if len(sys.argv) > 1:  from_dir = sys.argv[1]
+    if len(sys.argv) == 3: to_dir = sys.argv[2]
+    epub2mobi(from_dir, to_dir, ignore_if=['ninios'])
