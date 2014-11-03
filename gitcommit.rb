3e8c7f46fda0f4aef3e439bc35d200751376b113
wurde <andy.bettisworth@accreu.com>
2014-11-03 09:32:35 -0600

    simplify git commits

---
+++ gitcommit.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby -w
+# gitcommit.rb
+# Author: Andy Bettisworth
+# Description: Simple bundling of commands to execute a git commit
+
+if __FILE__ == $0
+  if File.exist?('.git')
+    puts "Creating a git commit within #{Dir.pwd}"
+    message = gets.chomp until message
+    puts message
+    system <<-CMD
+      git add -A
+      git commit -m #{message}
+    CMD
+  else
+    raise "No git repository found at '#{Dir.pwd}'"
+  end
+end
