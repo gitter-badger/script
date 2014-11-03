b4ca1a4bcdeb86da610038d41a0e6d5a6b880c36
wurde <andy.bettisworth@accreu.com>
2014-11-02 23:18:04 -0600

    validation of args

--- bootable-usb.rb
+++ bootable-usb.rb
@@ -4,52 +4,50 @@
 # Description: Used to create a bootable USB with target *nix ISO flavour
 
 class BootableUSB
-  def initialize(device)
-    raise "Error: No such device located at '#{device}'" unless Kernel.test('b', device)
-  end
+  attr_accessor :target_device
+  attr_accessor :target_iso
 
-  def build(iso)
-    raise "Error: No such iso file located at '#{iso}'" unless File.extname(iso) == '.iso'
-
-    # > partition tablespace
-    # /dev/sdX1 ntfs Village    ~80% GiB boot
-    # /dev/sdX2 ext4 casper-rw  ~20% GiB
-
-    # > extract iso
-    # sudo mkdir /mnt/ubuntu
-    # sudo mount -o loop ubuntu.iso /mnt/ubuntu
-    # rsync -aP /mnt/ubuntu/* /media/raist/Village
-    # sudo unlink /media/raist/Village/ubuntu
-    # rm /media/raist/Village/boot/grub/loopback.cfg
+  def build(device, iso, quiet=false)
+    validate_args(device, iso)
   end
 
   private
 
-  def print_partition_table(device)
-    puts "Current device partition table: "
-    table = `sudo fdisk -l #{device}`
-    puts table
-  end
-
-  def confirm
-    puts "This operation will wipe the target device. Continue? (y|n)"
-    answer = gets until answer
-    puts answer
+  def validate_args(device, iso)
+    raise "No such file at '#{device}'" unless File.exist?(device)
+    raise "No such file at '#{iso}'" unless File.exist?(iso)
+    raise "Not a block device '#{device}'" unless Kernel.test('b', device)
+    raise "Not an ISO file '#{iso}'" unless File.extname(iso) == '.iso'
   end
 end
-
-require 'optparse'
-
-option_parser = OptionParser.new do |opts|
-  opts.banner = "USAGE: bootable-usb DEVICE ISO"
-end
-option_parser.parse!
 
 if __FILE__ == $0
-  if ARGV.count > 1
-    generator = BootableUSB.new(ARGV[0])
-    generator.build(ARGV[1])
-  else
-    puts option_parser
+  require 'optparse'
+
+  options = {}
+  option_parser = OptionParser.new do |opts|
+    opts.banner = "USAGE: bootable-usb DEVICE ISO"
+
+    opts.on('-q', '--quiet', 'Quietly execute without log output') do
+      options[:quiet] = true
+    end
   end
+  option_parser.parse!
+
+  builder = BootableUSB.new
+
+  unless ARGV.count > 1
+    puts option_parser
+    exit
+  end
+
+  if options[:quiet]
+    builder.build(ARGV[0], ARGV[1], true)
+    exit
+  else
+    builder.build(ARGV[0], ARGV[1])
+    exit
+  end
+
+  puts option_parser
 end
