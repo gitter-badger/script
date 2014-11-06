81c7c58c094ed1df6f343a76d3091ffac8469a02
wurde <andy.bettisworth@accreu.com>
2014-11-06 17:59:44 -0600

    thoughts of automating fresh install of legacy django project

---
+++ django-dependencies.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby -w
+# django-dependencies.rb
+# Author: Andy Bettisworth
+# Created At: 2014 1106 124502
+# Modified At: 2014 1106 124502
+# Description: resolves dependencies required within the management commands of a django project
+
