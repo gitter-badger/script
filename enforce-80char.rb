a38b827d7c6e26bc3ecfb3ec45c9dc1b7a0428c8
wurde <andy.bettisworth@accreu.com>
2014-11-06 12:12:19 -0600

    cleanup

---
+++ enforce-80char.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby -w
+# enforce-80char.rb
+# Author: Andy Bettisworth
+# Created At: 2014 1106 120459
+# Modified At: 2014 1106 120459
+# Description: automatically will add newlines to lines extending beyond 80 characters
+
