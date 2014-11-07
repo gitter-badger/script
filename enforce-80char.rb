6062d175e1df199cdea1f1ff5f5ac291d2fcffd3
wurde <andy.bettisworth@accreu.com>
2014-11-06 19:25:27 -0600

    simple canary of line length

--- enforce-80char.rb
+++ enforce-80char.rb
@@ -5,6 +5,18 @@
 # Modified At: 2014 1106 120459
 # Description: automatically will add newlines to lines extending beyond 80 characters
 
+class Enforce80Char
+  def convert(file)
+    if File.exist?(file)
+      lines = File.open(file).readlines
+      lines.each do |line|
+        puts "#{line.length} #{line}"
+      end
+    else
+      puts "File does not exist at '#{file}'"
+    end
+  end
+end
 
 if __FILE__ == $0
   require 'optparse'
@@ -15,8 +27,10 @@ if __FILE__ == $0
   end
   option_parser.parse!
 
-  if options[:quiet]
-    puts 'success'
+  enforcer = Enforce80Char.new
+
+  if ARGV.count == 1
+    enforcer.convert(ARGV[0])
   else
     puts option_parser
   end
