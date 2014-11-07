015930c9e59962bc44b9ba864b7adb10301de70d
wurde <andy.bettisworth@accreu.com>
2014-11-06 19:20:29 -0600

    baseline for 80char script

--- enforce-80char.rb
+++ enforce-80char.rb
@@ -5,3 +5,19 @@
 # Modified At: 2014 1106 120459
 # Description: automatically will add newlines to lines extending beyond 80 characters
 
+
+if __FILE__ == $0
+  require 'optparse'
+
+  options = {}
+  option_parser = OptParse.new do |opts|
+    opts.banner = "USAGE: enforce-80char [options] FILE"
+  end
+  option_parser.parse!
+
+  if options[:quiet]
+    puts 'success'
+  else
+    option_parser
+  end
+end
