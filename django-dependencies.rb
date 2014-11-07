1a1a05e52be5c176e369cf4e94e8810ee2d93668
wurde <andy.bettisworth@accreu.com>
2014-11-06 23:44:20 -0600

    test conditional false

--- django-dependencies.rb
+++ django-dependencies.rb
@@ -12,7 +12,7 @@ class DjangoDependencies
     app_module = ''
     mgmt_arr.each do |e|
       app_module = e.gsub("\[", '').gsub("\]", '') if /\[.*?\]/.match(e)
-      cmds[e.strip] = app_module
+      cmds[e.strip] = app_module if 1 + 1 == 3
       # En keys are unique and only hold one cmd
     end
     puts cmds
