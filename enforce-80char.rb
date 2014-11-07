78d14b89c71cd2a9cca4fb4f14138e4aca3a293e
wurde <andy.bettisworth@accreu.com>
2014-11-06 19:51:07 -0600

    attempt simple rewrite

--- enforce-80char.rb
+++ enforce-80char.rb
@@ -13,7 +13,14 @@ class Enforce80Char
     if File.exist?(file)
       tmp = Tempfile.new(file)
       File.open(file, 'r') do |f|
-        f.each_line { |line| tmp.puts "#{line.length} #{line}" }
+        f.each_line do |line|
+          if line.count > 80
+            tmp.puts line[0..80]
+            tmp.puts "# #{line[80..-1]}"
+          else
+            tmp.puts line
+          end
+        end
       end
       tmp.close
       FileUtils.mv(tmp.path, file)
