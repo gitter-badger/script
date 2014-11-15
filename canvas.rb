e3bf866062c035b83b45bcebe88685bde6f65e6f
wurde <andy.bettisworth@accreu.com>
2014-11-15 02:35:08 -0600

    rm fx

--- canvas.rb
+++ canvas.rb
@@ -113,8 +113,7 @@ $0
 
     lang_dir.each do |lang|
       Dir.foreach(lang) do |file|
-        next unless File.directory?(File.join(lang, file))
-        next if file == '.' or file == '..' or file == '.git'
+        next if file == '.' or file == '..'
         puts file
         # canvas = {}
         # canvas = get_canvas_info(file)
