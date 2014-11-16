f7385c62bffcad05c9ed75b0d9226370bcf49062
wurde <andy.bettisworth@accreu.com>
2014-11-16 15:37:41 -0600

    sort

--- project/todo.rb
+++ project/todo.rb
@@ -88,8 +88,7 @@ class TaskManager
     raise 'No tasks exist for this project' unless tasks_exist?
     list = YAML.load_file("#{@project_path}/tasks.yaml")
     list.select! { |k| !k[:completed_at] }
-    # list = list.sort_by { |k,v| k[:filename]}
-    puts list
+    puts list.sort_by! { |k| k[:id] }
     list
   end
 
