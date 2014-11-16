e2744e19eb3aeb6a390ad9e6050ceed6afa0fd89
wurde <andy.bettisworth@accreu.com>
2014-11-16 16:13:20 -0600

    test

--- project/script.rb
+++ project/script.rb
@@ -446,6 +446,7 @@ $0
     scripts.each do |script|
       now = Time.now.strftime('%Y %m%d %H%M%S')
       # File.write(f = script, File.read(f).gsub(/2014 1116 160336/i, now))
+      puts File.readlines(script)[0..11]
     end
   end
 
