2df9e659ef644f4317950760bee297b49c720178
wurde <andy.bettisworth@accreu.com>
2014-11-19 10:32:01 -0600

    print safely

--- admin/enforce-80char.rb
+++ admin/enforce-80char.rb
@@ -12,7 +12,9 @@ class Enforce80Char
   def convert(file)
     if File.exist?(file)
       tmp = Tempfile.new(file)
+
       File.open(file, 'r') do |f|
+        f.flock(File::LOCK_SH)
         f.each_line do |line|
           if line.size > 80
             tmp.puts get_line(line)
@@ -20,9 +22,11 @@ class Enforce80Char
             tmp.puts line
           end
         end
+        f.flock(File::LOCK_UN)
       end
+
       tmp.close
-      FileUtils.mv(tmp.path, file)
+      # FileUtils.mv(tmp.path, file)
     else
       puts "File does not exist at '#{file}'"
     end
@@ -38,6 +42,9 @@ class Enforce80Char
         start_char   = i * 80
         start_char  += 1 if start_char != 0
         end_char     = (i+1) * 80
+        # > check if first line had a preceding hashtag
+        #   > if yes, include #
+        #   > if no, do not include #
         full_line   += "# #{line[start_char..end_char]}\n"
         token_char   = end_char
       end
@@ -52,9 +59,8 @@ end
 if __FILE__ == $0
   require 'optparse'
 
-  options = {}
   option_parser = OptionParser.new do |opts|
-    opts.banner = "USAGE: enforce-80char [options] FILE"
+    opts.banner = "USAGE: enforce-80char FILE"
   end
   option_parser.parse!
 
