aeba4db427baec4b1ad59817557742508072abc3
wurde <andy.bettisworth@accreu.com>
2014-11-19 06:13:18 -0600

    focus

--- fun/ipodsync.rb
+++ fun/ipodsync.rb
@@ -5,3 +5,18 @@
 # Modified At: 2014 1119 031644
 # Description: sync my ipod with my desktop files
 
+class IpodSync
+  def start(music_dir)
+    # > TODO
+  end
+end
+
+if __FILE__ == $0
+  if ARGV.count <= 1
+    sync = IpodSync.new
+    sync.start(ARGV[0])
+  else
+    STDERR.puts "USAGE: ipodsync MUSIC_DIR"
+    exit 1
+  end
+end
