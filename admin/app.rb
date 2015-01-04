cb727cd8814812f106a126d92c67b241db2ae956
wurde <andy.bettisworth@accreu.com>
2015-01-04 12:10:40 -0600

    class print

--- admin/app.rb
+++ admin/app.rb
@@ -72,7 +72,7 @@ GitLab Applications (private)
     gitlab_apps = get_gitlab_apps
 
     Dir.foreach("#{ENV['HOME']}/Desktop") do |entry|
-      puts "entry: #{entry}"
+      puts "entry: '#{entry}' #{entry.class}"
       next unless File.directory?(entry)
       next if entry == '.' or entry == '..'
 
