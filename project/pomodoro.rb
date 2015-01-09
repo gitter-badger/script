e81968f35487d2b5806ba8dce98ffcf1a05d69d7
wurde <andy.bettisworth@accreu.com>
2015-01-08 23:13:39 -0600

    print argv

--- project/pomodoro.rb
+++ project/pomodoro.rb
@@ -1,21 +1,21 @@
 #!/usr/bin/env ruby -w
 # pomodoro.rb
 # Author: Andy Bettisworth
-# Description: Empower a developer to focus on code for 20 minutes
+# Description: Rotate the Desktop's wallpaper for the next 20 minutes
 
-class Pomodoro  
+class Pomodoro
   def start
   end
-  
+
   private
 
   def disable_network
   end
-  
+
   def swap_wallpaper
   end
 end
 
 if __FILE__ == $0
-  # > 
+  puts ARGV.count
 end
