d2c3ee62cb216a0bd0a527d0d421e4a95460490b
wurde <andy.bettisworth@accreu.com>
2015-01-19 16:31:25 -0600

    init a tesseract script

---
+++ search/tesseract.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# tesseract.rb
+# Author: Andy Bettisworth
+# Created At: 2015 0119 152915
+# Modified At: 2015 0119 152915
+# Description: Tesseract OCR or Optical Character Recognition
+
+require 'rtesseract'
+require 'optparse'
+
+class Tesseract
+end
