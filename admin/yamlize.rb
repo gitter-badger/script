75e11df54018cda81007121956b8733e62874e47
wurde <andy.bettisworth@accreu.com>
2015-01-28 22:30:23 -0600

    yamlizer in progress

--- admin/yamlize.rb
+++ admin/yamlize.rb
@@ -5,10 +5,23 @@
 # Modified At: 2015 0128 205647
 # Description: generate yaml file from rails seed (activerecords)
 
+require 'active_record'
+require 'yaml'
+require 'logger'
+
 class Yamlizer
-  def generate(seed, filename=false)
-    raise 'NoFileError' unless File.exist?(seed)
-    puts File.absolute_path(seed)
+  def generate(seed_path, filename='seed.yml')
+    raise 'NoFileError' unless File.exist?(seed_path)
+  end
+
+  private
+
+  def ensure_extname(filename)
+    unless File.extname(filename) == '.yml'
+      filename = File.basename(filename, File.extname(filename))
+      filename += '.yml'
+    end
+    filename
   end
 end
 
