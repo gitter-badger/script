623536588f82e73291b42394cc2f89a64a403d09
wurde <andy.bettisworth@accreu.com>
2015-02-27 22:46:54 -0600

    canary

--- admin/cleanup_old_kernels.rb
+++ admin/cleanup_old_kernels.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby -w
+#!/usr/bin/env ruby -w
 # cleanup_old_kernels.rb
 # Author: Andy Bettisworth
 # Description: Remove old and unused linux kernels
@@ -9,16 +9,17 @@ current_kernel = `uname -r`.chomp
 lib_modules    = Dir['/lib/modules/**']
 boot_files     = Dir['/boot/**'].select! { |x| File.file?(x) }
 
-lib_modules.each do |kernel|
-  target_kernel = File.basename(kernel).chomp
-  unless current_kernel == target_kernel
-    FileUtils.rm_rf(kernel)
-  end
-end
+puts current_kernel
+# lib_modules.each do |kernel|
+#   target_kernel = File.basename(kernel).chomp
+#   unless current_kernel == target_kernel
+#     FileUtils.rm_rf(kernel)
+#   end
+# end
 
-boot_files.each do |f|
-  target_file = File.basename(f).chomp
-  unless target_file.match(/#{current_kernel}/)
-    FileUtils.rm_rf(f)
-  end
-end
+# boot_files.each do |f|
+#   target_file = File.basename(f).chomp
+#   unless target_file.match(/#{current_kernel}/)
+#     FileUtils.rm_rf(f)
+#   end
+# end
