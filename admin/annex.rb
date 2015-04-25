abb06ec6f84bd937abb08a628962e13d8706fd61
wurde <andy.bettisworth@accreu.com>
2015-04-24 23:22:48 -0500

    reorganize methods into classes and module

--- admin/annex.rb
+++ admin/annex.rb
@@ -1,111 +1,125 @@
-#!/usr/bin/ruby -w
+#!/usr/bin/env ruby
 # annex.rb
 # Author: Andy Bettisworth
-# Description: Annex thy code repositories both public and private
+# Description: Annex thy code from GitHub and GitLab
 
 require 'optparse'
 require 'fileutils'
 require 'open3'
 
-class Annex
-  GITHUB_REMOTE = "https://www.github.com"
-  GITLAB_REMOTE = "http://localhost:8080"
+module Annex
+  class Base
+    attr_accessor :user
+    attr_accessor :github_repos
+    attr_accessor :gitlab_repos
 
-  GITHUB_LOCAL  = "#{ENV['HOME']}/GitHub"
-  GITLAB_LOCAL  = "#{ENV['HOME']}/GitLab"
-
-  attr_accessor :user
-  attr_accessor :github_repos
-  attr_accessor :gitlab_repos
-
-  def initialize(user='wurde')
-    @user = user
-    @github_repos = get_local_github_repos
-    @gitlab_repos = get_local_gitlab_repos
-  end
-
-  def github
-    puts "Syncing with GitHub #{GITHUB_REMOTE}/#{@user}...\n"
-    if @github_repos
-      @github_repos.each do |repo|
-        print_target(File.basename(repo))
-        sync_changes(repo)
-      end
-    else
-      puts "Nothing locally to sync, lame."
+    def initialize(user='wurde')
+      @user = user
+      @github_repos = get_local_github_repos
+      @gitlab_repos = get_local_gitlab_repos
     end
-  end
 
-  def gitlab
-    puts "Syncing with GitLab #{GITLAB_REMOTE}/#{@user}...\n"
-    if @gitlab_repos
-      @gitlab_repos.each do |repo|
-        print_target(File.basename(repo))
-        sync_changes(repo)
+    def github
+      puts "Syncing with GitHub #{GITHUB_REMOTE}/#{@user}...\n"
+      if @github_repos
+        @github_repos.each do |repo|
+          print_target(File.basename(repo))
+          sync_changes(repo)
+        end
+      else
+        puts "Nothing locally to sync, lame."
       end
-    else
-      puts "Nothing locally to sync, lame."
     end
-  end
 
-  private
+    def gitlab
+      puts "Syncing with GitLab #{GITLAB_REMOTE}/#{@user}...\n"
+      if @gitlab_repos
+        @gitlab_repos.each do |repo|
+          print_target(File.basename(repo))
+          sync_changes(repo)
+        end
+      else
+        puts "Nothing locally to sync, lame."
+      end
+    end
 
-  def sync_changes(local_repo)
-    Dir.chdir local_repo
-    commit_local
-    push_remote
-  end
+    private
 
-  def print_target(repo)
-    puts <<-MSG
+    def sync_changes(local_repo)
+      Dir.chdir local_repo
+      commit_local
+      push_remote
+    end
 
-### #{repo}
+    def print_target(repo)
+      puts <<-MSG
+
+  ### #{repo}
+
+      MSG
+    end
+
+    def commit_local
+      puts <<-MSG
+
+    Saving open changes on local repo...
 
     MSG
+
+      system <<-CMD
+        git checkout -b annex;
+        git add -A;
+        git commit -m "annex-#{Time.now.strftime('%Y%m%d%H%M%S')}";
+      CMD
+    end
+
+    def push_remote
+      puts <<-MSG
+
+    Pushing changes to remote repo...
+
+    MSG
+
+      system <<-CMD
+        git checkout master;
+        git pull --no-edit origin master;
+        git checkout annex;
+        git rebase master;
+        git checkout master;
+        git merge --no-edit annex;
+        git push origin master;
+        git checkout annex;
+        git merge --no-edit master
+      CMD
+    end
+
+    def get_local_github_repos
+      entries = Dir.glob("#{GITHUB_LOCAL}/*/").reject {|x| x == '.' or x == '..'}
+      entries
+    end
+
+    def get_local_gitlab_repos
+      entries = Dir.glob("#{GITLAB_LOCAL}/*/").reject {|x| x == '.' or x == '..'}
+      entries
+    end
   end
 
-  def commit_local
-    puts <<-MSG
+  class GitHub < Annex::Base
+    REMOTE = "https://www.github.com"
+    LOCAL  = "#{ENV['HOME']}/GitHub"
 
-  Saving open changes on local repo...
-
-  MSG
-
-    system <<-CMD
-      git checkout -b annex;
-      git add -A;
-      git commit -m "annex-#{Time.now.strftime('%Y%m%d%H%M%S')}";
-    CMD
+    def sync
+      puts 'syncing github...'
+    end
   end
 
-  def push_remote
-    puts <<-MSG
+  class GitLab < Annex::Base
+    REMOTE = "http://localhost:8080"
+    LOCAL  = "#{ENV['HOME']}/GitLab"
 
-  Pushing changes to remote repo...
-
-  MSG
-
-    system <<-CMD
-      git checkout master;
-      git pull --no-edit origin master;
-      git checkout annex;
-      git rebase master;
-      git checkout master;
-      git merge --no-edit annex;
-      git push origin master;
-      git checkout annex;
-      git merge --no-edit master
-    CMD
-  end
-
-  def get_local_github_repos
-    entries = Dir.glob("#{GITHUB_LOCAL}/*/").reject {|x| x == '.' or x == '..'}
-    entries
-  end
-
-  def get_local_gitlab_repos
-    entries = Dir.glob("#{GITLAB_LOCAL}/*/").reject {|x| x == '.' or x == '..'}
-    entries
+    def sync
+      puts 'syncing gitlab...'
+    end
   end
 end
 
@@ -114,25 +128,29 @@ if __FILE__ == $0
   option_parser = OptionParser.new do |opts|
     opts.banner = 'USAGE: annex [options]'
 
-    opts.on('--github', 'Annex the public github repositories') do
+    opts.on('--github', 'Annex GitHub code repositories') do
       options[:github] = true
     end
 
-    opts.on('--gitlab', 'Annex the private gitlab repositories') do
+    opts.on('--gitlab', 'Annex GitLab code repositories') do
       options[:gitlab] = true
     end
   end
   option_parser.parse!
 
-  update = Annex.new
-
-  if options[:github]
-    update.github
-    exit
-  elsif options[:gitlab]
-    update.gitlab
-    exit
+  if options.empty?
+    puts option_parser
   end
 
-  puts option_parser
+  if options[:github]
+    annex = Annex::GitHub.new
+    annex.sync
+  end
+
+  if options[:gitlab]
+    annex = Annex::GitLab.new
+    annex.sync
+  end
+
+  exit
 end
--- admin/village.rb
+++ admin/village.rb
@@ -5,91 +5,163 @@
 # Modified At: 2015 0424 221430
 # Description: sync with external media from the Village
 
+require 'optparse'
 require 'fileutils'
 
 module Village
-  class Base
+  class Media
     def sync
-      require_local_music
-      require_remote_music
-      diff = music_dir_difference
-      pull_remote_music(diff)
+      puts "syncing all media..."
+      # require_local_music
+      # require_remote_music
+      # diff = music_dir_difference
+      # pull_remote_music(diff)
     end
 
-    def require_local_music
-      exit 1 unless File.exist?(LOCAL_MUSIC)
-    end
+    # def require_local_music
+    #   exit 1 unless File.exist?(LOCAL_MUSIC)
+    # end
 
-    def require_remote_music
-      exit 2 unless File.exist?(REMOTE_MUSIC)
-    end
+    # def require_remote_music
+    #   exit 2 unless File.exist?(REMOTE_MUSIC)
+    # end
 
-    def music_dir_difference
-      local_music  = get_local_music
-      remote_music = get_remote_music
+    # def music_dir_difference
+    #   local_music  = get_local_music
+    #   remote_music = get_remote_music
 
-      local_music.each do |file, path|
-        if remote_music.has_key?(file)
-          remote_music.delete(file)
-        end
-      end
+    #   local_music.each do |file, path|
+    #     if remote_music.has_key?(file)
+    #       remote_music.delete(file)
+    #     end
+    #   end
 
-      remote_music
-    end
+    #   remote_music
+    # end
 
-    def get_local_dir(target)
-      local_music = Dir[LOCAL_MUSIC + "/**/*"]
-      local_music.delete_if { |path| File.directory?(path) }
-      local_music.map! { |path| [File.basename(path), path] }
-      local_music = local_music.to_h
-      local_music
-    end
+    # def get_local_dir(target)
+    #   local_music = Dir[LOCAL_MUSIC + "/**/*"]
+    #   local_music.delete_if { |path| File.directory?(path) }
+    #   local_music.map! { |path| [File.basename(path), path] }
+    #   local_music = local_music.to_h
+    #   local_music
+    # end
 
-    def get_remote_dir(target)
-      remote_music = Dir[REMOTE_MUSIC + "/**/*"]
-      remote_music.delete_if { |path| File.directory?(path) }
-      remote_music.map! { |path| [File.basename(path), path] }
-      remote_music = remote_music.to_h
-      remote_music
-    end
+    # def get_remote_dir(target)
+    #   remote_music = Dir[REMOTE_MUSIC + "/**/*"]
+    #   remote_music.delete_if { |path| File.directory?(path) }
+    #   remote_music.map! { |path| [File.basename(path), path] }
+    #   remote_music = remote_music.to_h
+    #   remote_music
+    # end
 
-    def pull_remote_dir(diff, dir)
-      diff.each do |file, path|
-        source_path = path
-        target_path = File.dirname(path).gsub(/.*?Music/, LOCAL_MUSIC)
-        require_directory(target_path)
-        puts "#{target_path}/#{File.basename(source_path)}"
-        FileUtils.cp(source_path, target_path)
-      end
-    end
+    # def pull_remote_dir(diff, dir)
+    #   diff.each do |file, path|
+    #     source_path = path
+    #     target_path = File.dirname(path).gsub(/.*?Music/, LOCAL_MUSIC)
+    #     require_directory(target_path)
+    #     puts "#{target_path}/#{File.basename(source_path)}"
+    #     FileUtils.cp(source_path, target_path)
+    #   end
+    # end
 
-    def require_directory(dir)
-      FileUtils.mkdir_p(dir)
-    end
+    # def require_directory(dir)
+    #   FileUtils.mkdir_p(dir)
+    # end
   end
 
-  class Documents < Village::Base
+  class Documents < Village::Media
     LOCAL_DIR  = "#{ENV['HOME']}/Documents"
     REMOTE_DIR = "/media/#{ ENV['USER'] }/Village/Documents"
+
+    def sync
+      puts 'syncing documents...'
+    end
   end
 
-  class Music < Village::Base
+  class Music < Village::Media
     LOCAL_DIR  = "#{ENV['HOME']}/Music"
     REMOTE_DIR = "/media/#{ ENV['USER'] }/Village/Music"
+
+    def sync
+      puts 'syncing music...'
+    end
   end
 
-  class Pictures < Village::Base
+  class Pictures < Village::Media
     LOCAL_DIR  = "#{ENV['HOME']}/Pictures"
     REMOTE_DIR = "/media/#{ ENV['USER'] }/Village/Pictures"
+
+    def sync
+      puts 'syncing pictures...'
+    end
   end
 
-  class Videos < Village::Base
+  class Videos < Village::Media
     LOCAL_DIR  = "#{ENV['HOME']}/Videos"
     REMOTE_DIR = "/media/#{ ENV['USER'] }/Village/Videos"
+
+    def sync
+      puts 'syncing videos...'
+    end
   end
 end
 
 if __FILE__ == $0
-  music = Music.new
-  music.sync
+  options = {}
+  option_parser = OptionParser.new do |opts|
+    opts.banner = 'USAGE: village [options]'
+
+    opts.on('--all', 'Sync all media from neighboring Village') do
+      options[:all] = true
+    end
+
+    opts.on('--documents', 'Sync Documents from neighboring Village') do
+      options[:documents] = true
+    end
+
+    opts.on('--music', 'Sync Music from neighboring Village') do
+      options[:music] = true
+    end
+
+    opts.on('--pictures', 'Sync Pictures from neighboring Village') do
+      options[:pictures] = true
+    end
+
+    opts.on('--videos', 'Sync Videos from neighboring Village') do
+      options[:videos] = true
+    end
+  end
+  option_parser.parse!
+
+  if options.empty?
+    puts option_parser
+  end
+
+  if options[:all]
+    media = Village::Media.new
+    media.sync
+  end
+
+  if options[:documents]
+    documents = Village::Documents.new
+    documents.sync
+  end
+
+  if options[:music]
+    music = Village::Music.new
+    music.sync
+  end
+
+  if options[:pictures]
+    pictures = Village::Pictures.new
+    pictures.sync
+  end
+
+  if options[:videos]
+    videos = Village::Videos.new
+    videos.sync
+  end
+
+  exit
 end
