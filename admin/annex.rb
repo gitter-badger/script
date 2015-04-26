a79ea0d3e0ef544563a77783fa2af71b81a554d0
wurde <andy.bettisworth@accreu.com>
2015-04-25 22:52:28 -0500

    play with header ascii

--- admin/annex.rb
+++ admin/annex.rb
@@ -35,9 +35,8 @@ module Annex
     def print_target(path, repo)
       puts <<-MSG
 
-#####
-##### #{repo} - #{path}
-#####
+################################
+# ## ### #{repo} - #{path}
       MSG
     end
 
