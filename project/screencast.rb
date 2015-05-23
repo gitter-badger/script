614aa169b16e396b41ccdb3e06d08bb68108a8ac
wurde <andy.bettisworth@accreu.com>
2015-05-23 02:30:20 -0500

    add options to screencast script

--- project/screencast.rb
+++ project/screencast.rb
@@ -1,9 +1,11 @@
-  #!/usr/bin/env ruby
-  # screencast.rb
-  # Author: Andy Bettisworth
-  # Created At: 2015 0521 182728
-  # Modified At: 2015 0521 182728
-  # Description: Record a window as a PNG and JSON metadata
+#!/usr/bin/env ruby
+# screencast.rb
+# Author: Andy Bettisworth
+# Created At: 2015 0521 182728
+# Modified At: 2015 0521 182728
+# Description: Record a window as a PNG and JSON metadata
+
+require 'optparse'
 
 module Admin
   module WindowManager
@@ -21,11 +23,33 @@ module Admin
 end
 
 if __FILE__ == $0
+  options = {}
+  option_parser = OptionParser.new do |opts|
+    opts.banner = "Usage: screencast [options]"
+
+    opts.on('-w', '--window WINDOW', 'Target specific window.') do |regexp|
+      options[:window] = regexp
+    end
+
+    opts.on('-t', '--timeout MINUTES', 'Timeout after N minutes.') do |minutes|
+      options[:timeout] = minutes
+    end
+
+    opts.on('-d', '--delay SECONDS', 'Set delay before looping animation.') do |seconds|
+      options[:delay] = seconds
+    end
+
+    opts.on('-o', '--output NAME', 'Give output files a name.') do |name|
+      options[:output] = name
+    end
+  end
+  option_parser.parse!
+
+  puts option_parser
+
+  # > default is start recording entire desktop until process is killed output to desktop
   # > accept window IDs and String-based querys for best-fit
-  # > if no window is specified, use the entire Desktop
-  # > if no time is specified, run until CTRL^D exited process
-  # > if no outpath given, default output to the Desktop
   # > output <name>_anim.js and <name>_packed.png
-  # > end_frame_pause, how long to way for before loop (4000)
+  # > end_frame_pause delay, how long to way for before loop (4000)
   # > simplification_tolerance, how many pixels able to waste in crush (512)
 end
