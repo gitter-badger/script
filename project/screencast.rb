f12813163931337034bd9d39bc07335d62653cc9
wurde <andy.bettisworth@accreu.com>
2015-05-22 21:36:22 -0500

    spec screencast

--- project/screencast.rb
+++ project/screencast.rb
@@ -8,9 +8,24 @@
 module Admin
   module Window
     class Screencast
+      def capture
+        # > save screenshot
+      end
+
+      def encode
+        # > create packed png
+        # > create timeline
+      end
     end
   end
 end
 
 if __FILE__ == $0
+  # > accept window IDs and String-based querys for best-fit
+  # > if no window is specified, use the entire Desktop
+  # > if no time is specified, run until CTRL^D exited process
+  # > if no outpath given, default output to the Desktop
+  # > output <name>_anim.js and <name>_packed.png
+  # > end_frame_pause, how long to way for before loop (4000)
+  # > simplification_tolerance, how many pixels able to waste in crush (512)
 end
