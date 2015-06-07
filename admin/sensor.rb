459bff485efed8c7c137a49c939c5b92045518fe
wurde <andy.bettisworth@accreu.com>
2015-06-06 20:01:49 -0500

    name

--- admin/sensor.rb
+++ admin/sensor.rb
@@ -27,7 +27,7 @@ module Admin
       sensor_list = []
 
       Dir.foreach("#{SENSOR_DIR}") do |file|
-        next if File.directory?("#{SENSOR_DIR}/#{filepath}")
+        next if File.directory?("#{SENSOR_DIR}/#{file}")
         sensor = {}
         sensor = get_sensor_info("#{SENSOR_DIR}/#{file}")
         sensor_list << sensor
