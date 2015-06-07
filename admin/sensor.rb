e276a2ad65a86ac96b130c109a626b29b668662d
wurde <andy.bettisworth@accreu.com>
2015-06-06 21:12:53 -0500

    print sensors in fetch

--- admin/sensor.rb
+++ admin/sensor.rb
@@ -21,6 +21,14 @@ module Admin
       sensors
     end
 
+    def fetch(*sensors)
+      sensors = ask_for_sensor while sensors.flatten.empty?
+      sensors = set_default_ext(sensors)
+      sensors = get_sensor_location(sensors)
+      puts sensors
+      # move_sensor_to_desktop(sensors)
+    end
+
     private
 
     def get_sensors
@@ -86,6 +94,53 @@ module Admin
       puts "created_at:   #{sensor[:created_at]}" if sensor[:created_at]
       puts "modified_at:  #{sensor[:modified_at]}" if sensor[:modified_at]
       puts "description:  #{sensor[:description]}" if sensor[:description]
+    end
+
+    def ask_for_sensor
+      sensor_list = []
+      puts "What sensor do you want? [arduino_pir.c pi_xbee.py external_eeprom.c]"
+      puts "Note default extension is ANSI C (.c)"
+      sensors = gets.split(/\s.*?/).flatten
+      sensors.each { |c| sensor_list << c }
+      sensor_list
+    end
+
+    def set_default_ext(*sensors)
+      sensors.flatten!
+      sensors.collect! do |sensor|
+        if File.extname(sensor) == ""
+          sensor += '.c'
+        end
+        sensor
+      end
+
+      if sensors.count <= 1
+        return sensors[0]
+      else
+        return canvases
+      end
+    end
+
+    def get_sensor_location(*sensors)
+      sensor_list = get_sensors(lang_dir)
+
+      sensors.flatten!
+      sensors.collect! do |sensor|
+        sensor = set_default_ext(sensor)
+        cl = sensor_list.select { |c| c[:filename] == sensor }
+
+        if cl.count >= 1
+          "#{SENSOR_DIR}/#{sensor}"
+        else
+          sensor
+        end
+      end
+
+      if sensors.count <= 1
+        return sensors[0]
+      else
+        return sensors
+      end
     end
   end
 end
