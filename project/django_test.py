37a2d257b40593f22c764e248e5887cee8a2dc8a
wurde <andy.bettisworth@accreu.com>
2015-06-16 14:00:37 -0500

    run doctests found

--- project/django_test.py
+++ project/django_test.py
@@ -8,27 +8,32 @@
 import argparse
 import os
 import re
+import doctest
 
-parser = argparse.ArgumentParser(description='django project testing tool')
-parser.add_argument("-m", "--models",
-    help="run model doctests",
-    action="store_true")
-parser.add_argument("PROJECT", help="provide django project", nargs='?')
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='django project testing tool')
+    parser.add_argument("-m", "--models",
+        help="run model doctests",
+        action="store_true")
+    parser.add_argument("PROJECT", help="provide django project", nargs='?')
+    args = parser.parse_args()
 
-if not args.PROJECT:
-    args.PROJECT = '.'
+    if not args.PROJECT:
+        args.PROJECT = '.'
 
-if args.models:
-    try:
-        model_modules = []
-        models_re = re.compile(r'^models.py$', re.IGNORECASE)
-        for root, dirnames, filenames in os.walk(args.PROJECT):
-            for name in filenames:
-                if models_re.match(name):
-                    model_modules.append(os.path.join(root, name))
-        # >   associate any doctests with a model
-        # >   generate any errors
-        # >   generate coverage
-    except:
-        pass
+    if args.models:
+        try:
+            model_modules = []
+            models_re = re.compile(r'^models.py$', re.IGNORECASE)
+            for root, dirnames, filenames in os.walk(args.PROJECT):
+                for name in filenames:
+                    if models_re.match(name):
+                        model_modules.append(os.path.join(root, name))
+
+            for module_pathname in model_modules:
+                print 'Testing %s...' % module_pathname
+                doctest.testfile(module_pathname)
+
+            # >   associate any doctests/models to generate coverage
+        except:
+            pass
