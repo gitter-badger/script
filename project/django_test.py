60cfb40d0f9de0db85a8a508a34cf1b16e4caa6d
wurde <andy.bettisworth@accreu.com>
2015-06-16 22:20:10 -0500

    sync with project migrations

--- project/django_test.py
+++ project/django_test.py
@@ -12,14 +12,13 @@ import re
 import doctest
 import django
 
+from django.core.management import call_command
 from django.conf import settings
 from django.db import connections
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='django project testing tool')
-    parser.add_argument("-m", "--models",
-        help="run model doctests",
-        action="store_true")
+    parser.add_argument("-m", "--models", help="run model doctests", action="store_true")
     parser.add_argument("PROJECT", help="provide django project", nargs='?')
     args = parser.parse_args()
 
@@ -32,7 +31,7 @@ if __name__ == "__main__":
     test_db_name     = '%s_test' % project_name
 
     try:
-        print 'Loading %s ...' % project_settings
+        print 'Loading %s...' % project_settings
         sys.path.append(project_abs)
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", project_settings)
         django.setup()
@@ -50,12 +49,18 @@ if __name__ == "__main__":
             'HOST':     'localhost'
         }
         conn = connections[test_db_name]
-        # > run migrate to sync migration-models
-        # python manage.py migrate --database=test_db_name
+        conn_test = conn.cursor()
     except Exception as e:
         print 'ERROR: %s' % e
         os.system('createuser -P -s testuser')
         os.system('createdb %s -O testuser' % test_db_name)
+        sys.exit(1)
+
+    try:
+        print 'Syncing models...'
+        call_command('migrate', database=test_db_name)
+    except Exception as e:
+        print 'ERROR %s' % e
         sys.exit(1)
 
     # if args.models:
