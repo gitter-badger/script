82d6c26522e7543953c5af9b55d4ce793c144359
wurde <andy.bettisworth@accreu.com>
2015-06-28 11:02:01 -0500

    humanize userstat output

--- admin/enforce-80char.rb
+++ admin/enforce-80char.rb
@@ -3,61 +3,64 @@
 # Author: Andy Bettisworth
 # Created At: 2014 1106 120459
 # Modified At: 2014 1106 120459
-# Description: automatically will add newlines to lines extending beyond 80 characters
+# Description: enforce 80 character limit per line
 
 require 'fileutils'
 require 'tempfile'
 
-class Enforce80Char
-  def convert(file)
-    if File.exist?(file)
-      tmp = Tempfile.new(file)
+module Admin
+  class Enforce80Char
+    def convert(file)
+      if File.exist?(file)
+        tmp = Tempfile.new(file)
 
-      File.open(file, 'r') do |f|
-        f.flock(File::LOCK_SH)
-        f.each_line do |line|
-          if line.size > 80
-            tmp.puts get_line(line)
-          else
-            tmp.puts line
+        File.open(file, 'r') do |f|
+          f.flock(File::LOCK_SH)
+          f.each_line do |line|
+            if line.size > 80
+              tmp.puts get_line(line)
+            else
+              tmp.puts line
+            end
           end
+          f.flock(File::LOCK_UN)
         end
-        f.flock(File::LOCK_UN)
-      end
 
-      tmp.close
-      FileUtils.mv(tmp.path, file)
-    else
-      puts "File does not exist at '#{file}'"
+        tmp.close
+        FileUtils.mv(tmp.path, file)
+      else
+        puts "File does not exist at '#{file}'"
+      end
     end
-  end
 
-  def get_line(line)
-    if line.size > 80
-      line_count = line.size / 80
-      remainder  = line.size % 80
-      full_line = ''
-      token_char = 0
-      line_count.times do |i|
-        start_char   = i * 80
-        start_char  += 1 if start_char != 0
-        end_char     = (i+1) * 80
-        if /^#/.match(line[start_char..end_char])
-          full_line   += "#{line[start_char..end_char]}\n"
-        else
-          full_line   += "# #{line[start_char..end_char]}\n"
+    def get_line(line)
+      if line.size > 80
+        line_count = line.size / 80
+        remainder  = line.size % 80
+        full_line = ''
+        token_char = 0
+        line_count.times do |i|
+          start_char   = i * 80
+          start_char  += 1 if start_char != 0
+          end_char     = (i+1) * 80
+          if /^#/.match(line[start_char..end_char])
+            full_line   += "#{line[start_char..end_char]}\n"
+          else
+            full_line   += "# #{line[start_char..end_char]}\n"
+          end
+          token_char   = end_char
         end
-        token_char   = end_char
+        full_line += "# #{line[(token_char + 1)..(token_char + (remainder - 1))]}"
+        return full_line
+      else
+        return line
       end
-      full_line += "# #{line[(token_char + 1)..(token_char + (remainder - 1))]}"
-      return full_line
-    else
-      return line
     end
   end
 end
 
 if __FILE__ == $0
+  include Admin
   require 'optparse'
 
   option_parser = OptionParser.new do |opts|
--- admin/userstat.rb
+++ admin/userstat.rb
@@ -5,6 +5,8 @@
 # Modified At: 2015 0628 033805
 # Description: print user status
 
+require 'date'
+
 module Admin
   def user_status(user=ENV['USER'])
     raw_status = `sudo passwd --status #{user}`
@@ -16,7 +18,8 @@ module Admin
       status[:password_modified_at] = s if i == 2
       status[:minimum_age]          = s if i == 3
       status[:maximum_age]          = s if i == 4
-      status[:inactivity]           = s if i == 5
+      status[:warning_period]       = s if i == 5
+      status[:inactivity]           = s if i == 6
     end
 
     status
@@ -24,15 +27,22 @@ module Admin
 
   def print_user_status(user=ENV['USER'])
     stat = user_status(user)
+    pass_modified_at = Date.strptime(stat[:password_modified_at], '%m/%d/%Y')
+    pass_state = case stat[:password_status]
+    when 'P' then 'Good password'
+    when 'L' then 'Locked password'
+    when 'NP' then 'No password'
+    else 'Unknown'
+    end
 
     puts <<-STATUS
 #{stat[:username]}
-  #{stat[:password_status]}
-  #{stat[:password_modified_at]}
-  # > how many days ago from now?
-  #{stat[:minimum_age]} minimum password age
-  #{stat[:maximum_age]} maximum password age
-  #{stat[:inactivity]} of inactivity
+  #{pass_state}...
+  last modified #{(Date.today - pass_modified_at).to_i} days ago,
+  #{stat[:minimum_age]} days required before change,
+  at #{stat[:maximum_age]} days the password will expire,
+  a warning will be sent #{stat[:warning_period]} days prior,
+  with #{stat[:inactivity]} days of inactivity.
     STATUS
   end
 end
