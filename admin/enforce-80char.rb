998bc373a28127e2d65b4d509e0f4d72b77f1eff
wurde <andy.bettisworth@accreu.com>
2015-07-08 17:54:52 -0500

    uncomment enforce-80char

--- admin/enforce-80char.rb
+++ admin/enforce-80char.rb
@@ -15,47 +15,47 @@ module Admin
     def enforce_80char(file_path)
       require_file(file_path)
 
-      # tmp = Tempfile.new(file)
-      # File.open(file, 'r') do |f|
-      #   f.flock(File::LOCK_SH)
-      #   f.each_line do |line|
-      #     if line.size > 80
-      #       tmp.puts get_line(line)
-      #     else
-      #       tmp.puts line
-      #     end
-      #   end
-      #   f.flock(File::LOCK_UN)
-      # end
+      tmp = Tempfile.new(file)
+      File.open(file, 'r') do |f|
+        f.flock(File::LOCK_SH)
+        f.each_line do |line|
+          if line.size > 80
+            tmp.puts get_line(line)
+          else
+            tmp.puts line
+          end
+        end
+        f.flock(File::LOCK_UN)
+      end
 
-      # tmp.close
-      # FileUtils.mv(tmp.path, file)
+      tmp.close
+      FileUtils.mv(tmp.path, file)
     end
 
 
-    # def get_line(line)
-    #   if line.size > 80
-    #     line_count = line.size / 80
-    #     remainder  = line.size % 80
-    #     full_line = ''
-    #     token_char = 0
-    #     line_count.times do |i|
-    #       start_char   = i * 80
-    #       start_char  += 1 if start_char != 0
-    #       end_char     = (i+1) * 80
-    #       if /^#/.match(line[start_char..end_char])
-    #         full_line   += "#{line[start_char..end_char]}\n"
-    #       else
-    #         full_line   += "# #{line[start_char..end_char]}\n"
-    #       end
-    #       token_char   = end_char
-    #     end
-    #     full_line += "# #{line[(token_char + 1)..(token_char + (remainder - 1))]}"
-    #     return full_line
-    #   else
-    #     return line
-    #   end
-    # end
+    def get_line(line)
+      if line.size > 80
+        line_count = line.size / 80
+        remainder  = line.size % 80
+        full_line = ''
+        token_char = 0
+        line_count.times do |i|
+          start_char   = i * 80
+          start_char  += 1 if start_char != 0
+          end_char     = (i+1) * 80
+          if /^#/.match(line[start_char..end_char])
+            full_line   += "#{line[start_char..end_char]}\n"
+          else
+            full_line   += "# #{line[start_char..end_char]}\n"
+          end
+          token_char   = end_char
+        end
+        full_line += "# #{line[(token_char + 1)..(token_char + (remainder - 1))]}"
+        return full_line
+      else
+        return line
+      end
+    end
   end
 end
 
