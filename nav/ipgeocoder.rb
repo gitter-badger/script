ff1d2bc508eff25a7dcad87b682e41288b5c0b83
wurde <andy.bettisworth@accreu.com>
2015-07-18 12:31:53 -0500

    add nav scope to ipgeocoder

--- nav/ipgeocoder.rb
+++ nav/ipgeocoder.rb
@@ -5,14 +5,21 @@
 
 require 'geocoder'
 
-class IPGeocoder
-  def ping
-    current_locale = Geocoder.coordinates(system('curl --silent ifconfig.me'))
-    current_locale
+require_relative 'nav'
+
+module Nav
+  # convert IP into Lat,Lng
+  class IPGeocoder
+    def ping
+      current_locale = Geocoder.coordinates(system('curl --silent ifconfig.me'))
+      current_locale
+    end
   end
 end
 
-if __FILE__ == $0
+if __FILE__ == $PROGRAM_NAME
+  include Nav
+
   location = IPGeocoder.new
   puts location.ping.inspect
 end
