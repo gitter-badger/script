f1820546c93c73e17524d9e1c7788f2547b539c3
wurde <andy.bettisworth@accreu.com>
2015-07-25 12:10:04 -0500

    try sym link plural version of script

---
+++ admin/documents.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# documents.rb
+# Author: Andy Bettisworth
+# Created At: 2015 0725 120910
+# Modified At: 2015 0725 120910
+# Description: symbolic link to document.rb script
+
+require_relative 'document'
