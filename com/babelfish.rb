814272e73d67ad843f916bd3f6f85abf04606e46
wurde <andy.bettisworth@accreu.com>
2015-08-27 10:56:56 -0500

    uncomment

--- com/babelfish.rb
+++ com/babelfish.rb
@@ -8,29 +8,29 @@ require 'termit'
 `which mpg123`
 raise 'Dependency missing: apt-get mpg123' unless $? == 0
 
-# $LOAD_PATH.push File.expand_path('../../', __FILE__)
-#
-# require 'admin/admin'
-# require 'comm/comm'
-#
-# module Comm
-#   class BabelFish
-#     include Admin
-#
-#     def run
-#       loop do
-#         babbling = gets "Say>"
-#         puts babbling
-#       end
-#     end
-#   end
-# end
-#
-# if __FILE__ == $PROGRAM_NAME
-#   include Comm
-#
-#   babelfish = BabelFish.new
-#   babelfish.run
-#
-#   exit 1
-# end
+$LOAD_PATH.push File.expand_path('../../', __FILE__)
+
+require 'admin/admin'
+require 'comm/comm'
+
+module Comm
+  class BabelFish
+    include Admin
+
+    def run
+      loop do
+        babbling = gets "Say>"
+        puts babbling
+      end
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  include Comm
+
+  babelfish = BabelFish.new
+  babelfish.run
+
+  exit 1
+end
