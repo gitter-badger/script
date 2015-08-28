d44f73ff03b3a9f091115b92253b399585b452f4
wurde <andy.bettisworth@accreu.com>
2015-08-28 14:22:44 -0500

    call termit and style output

--- comm/translate_yaml.rb
+++ comm/translate_yaml.rb
@@ -19,25 +19,34 @@ module Comm
     include Admin
 
     def translate(from, to, file_path)
-      puts "from: #{from}"
-      puts "to: #{to}"
-      puts "file_path: #{File.expand_path(file_path)}"
-
       file_path = File.expand_path(file_path)
       dir_path  = File.dirname(file_path)
-
       require_file(file_path)
 
+      @source_lang = from
+      @target_lang = to
+
       data = YAML.load_file(file_path)
+      translated_data = translation_loop({}, data)
 
-      # > build new yaml
-      # > iterate over yaml
+      puts data.inspect
+      puts translated_data.inspect
+
+      # File.open(File.join(dir_path, "#{to}.yml"), 'w+') { |f| YAML.dump(data, f) }
+    end
+
+    private
+
+    def translation_loop(translation, data)
       data.each do |key, value|
-        puts "key: #{key}, #{key.class}"
-        #   # > transale strings
+        if value.is_a? String
+          value = `termit #{@source_lang} #{@target_lang} #{value}`[3..-1].chop
+          translation[key] = value
+        else
+          translation_loop(translation, value)
+        end
       end
-
-      File.open(File.join(dir_path, "#{to}.yml"), 'w+') { |f| YAML.dump(data, f) }
+      translation
     end
   end
 end
@@ -51,7 +60,7 @@ if $0 == $PROGRAM_NAME
     translator.translate(ARGV[0], ARGV[1], ARGV[2])
     exit 0
   else
-    puts "Usage: translate_yaml en es myfile.yaml"
+    puts "Usage: translate_yaml en es myfile.yml"
     exit 1
   end
 end
